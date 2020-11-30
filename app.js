84eb6855160a82090fd0e2571d69a925aedfee38
vbaobao <vbao.main@gmail.com>
2020-11-29 17:30:28 -0800

    Functional GroceryList component

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <ul>
+      <li>persimmon</li>
+      <li>pot</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
